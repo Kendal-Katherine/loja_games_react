1f363abcf74c8d77d84116eda54cae8cd109734b
Kendal Katherine <kendal.katherine3@gmail.com>
2024-01-23 16:14:42 -0300

    Adicionando Rotas ao Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,23 +1,27 @@
+import { Link } from "react-router-dom";
 
 
 function Navbar() {
- 
-  
+
+
 
   return (
     <>
-     <div className='w-full bg-slate-500 text-white flex justify-center py-4'>
-          <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase'>Game Store</div>
+      <div className='w-full bg-slate-400 
+                flex justify-center py-4'>
 
-            <div className='flex gap-4'>
-              <div className='hover:underline'>Produtos</div>
-              <div className='hover:underline'>Categorias</div>
-              <div className='hover:underline'>Cadastrar Categoria</div>
-              
-            </div>
+        <div className="container flex justify-between text-lg">
+          <Link to='/home' className="text-2xl font-bold">Game Store</Link>
+
+          <div className='flex gap-4'>
+            <Link to='/home' className='hover:underline'>Produtos</Link>
+            <Link to='/categorias' className='hover:underline'>Categorias</Link>
+            <Link to='/cadastrarcategoria' className='hover:underline'>Cadastrar Categoria</Link>
+            <Link to='' className='hover:underline'>Sair</Link>
+
           </div>
         </div>
+      </div>
     </>
   )
 }
